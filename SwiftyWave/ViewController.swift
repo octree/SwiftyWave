6f798f548244701014755afa39f02f99c092380c
Octree <octree@octreedemacbook-pro.local>
2016-10-24 12:52:55 +0800

    Initial Commit

---
+++ SwiftyWave/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyWave
+//
+//  Created by Octree on 2016/10/24.
+//  Copyright © 2016年 Octree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
